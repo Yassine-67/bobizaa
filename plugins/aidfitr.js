620cc4ca89fad24abe9966dcd6b529f2f90700e8
Yassine-67 <163153517+Yassine-67@users.noreply.github.com>
2024-03-22 23:33:30 +0000

    Update aidfitr.js

--- plugins/aidfitr.js
+++ plugins/aidfitr.js
@@ -2,15 +2,15 @@ import fetch from 'node-fetch';
 
 const handler = async (m, { conn, args, text, command, usedPrefix, isCreator, isPrems }) => {
   let response = args.join(' ').split('|');
-  if (!args[0]) return conn.reply(m.chat, `ارسل تهنئة عيد الفطر لصديقك مثال :\n\n*${usedPrefix}${command}  noureddine ♥*`, m);
+  if (!args[0]) return conn.reply(m.chat, `ارسل تهنئة عيد الفطر لصديقك مثال :\n\n*${usedPrefix}${command}  Essaouidi ♥*`, m);
   conn.sendMessage(m.chat, {
     react: {
-      text: '🕒',
+      text: '👻',
       key: m.key,
     }
   });
   let res = `https://api.lolhuman.xyz/api/idulfitri?apikey=Gatadios&text=${response[0]}`;
-  conn.sendFile(m.chat, res, 'idulfitri.jpg', 'instagram.com/noureddine_ouafy', m, false);
+  conn.sendFile(m.chat, res, 'idulfitri.jpg', 'instagram.com/essaouidi_yassine', m, false);
 };
 handler.help = ['aidfitr'];
 handler.tags = ['logo'];
